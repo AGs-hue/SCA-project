cc3dcca0796e20d43f9099bc9607c9a8c3d5453e
AGs-hue <agnesarong@gmail.com>
2021-10-28 17:35:25 +0100

    update repo

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/home/ags/Documents.json")
+
+  project = "sca-project-330409"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
