cfd2a4d39d5b339d3d7b2dac2b3282751c4723ff
AGs-hue <agnesarong@gmail.com>
2021-10-28 15:15:17 +0100

    update repo

---
+++ infrastructure/main.tf
@@ -0,0 +1,56 @@
+# SPECIFY THE PROVIDER (GCP)
+provider "google" {
+  version     = "3.5.0"
+  credentials = file("/Downloads/sca-project-instance.json") 
+  project     = "sca-project"
+  region      = "us-central1"
+  zone        = "us-central-a"
+}
+
+# resource "google_compute_network" "vpc_network" {
+#   name = "terraform-network"
+# }
+
+resource "google_compute_instance" "myinstance" {
+  name         = "virtual-machine-from-terraform"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # GIVE VM EXTERNAL IP ADDRESS
+    }
+  }
+
+  
+  #  APPLY FIREWALL RULES TO ALLOW EXTERNAL IPs TO ACCESS THIS INSTANCE
+  tags = ["http-server"]
+}
+
+resource "google_compute_firewall" "http-server" {
+  metadata_startup_script = "echo /www/html/index.html"
+
+  name = "default-allow-http-terraform"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports = ["80"]
+  }
+
+  # ALLOW TRAFFIC TO WITH HTTP-SERVER TAG TO INSTANCES
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["http-server"]
+}
+
+output "ip" {
+  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat.ip}"
+}
